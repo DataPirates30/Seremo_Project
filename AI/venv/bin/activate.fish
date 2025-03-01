d61531a0cea17e46123b2bebcd9ae4fcf7427fd4
DataPirates30 <sumanthegreat30@gmail.com>
2025-03-01 06:24:06 -0600

    testing Phase1 Completed

--- AI/venv/bin/activate.fish
+++ AI/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/suman-acharya/Seremo_Project/ai/venv
+set -gx VIRTUAL_ENV /home/suman-acharya/Seremo_Project/AI/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
